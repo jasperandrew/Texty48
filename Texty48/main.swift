d609220ea11388137e9ff33811b2d9f2ef18b994
Jasper Andrew <jasperandrew@iFake.local>
2017-02-06 01:17:28 -0500

    Game basically functional, except for gameover

--- Texty48/main.swift
+++ Texty48/main.swift
@@ -7,19 +7,27 @@
 //
 
 import Foundation
-let DBG = false
 
-var board = [[1,2,0,0],
-             [0,2,0,0],
-             [0,0,2,0],
+// The 2D array storing the board's values.
+var board = [[0,0,0,0],
+             [0,0,0,0],
+             [0,0,0,0],
              [0,0,0,0]]
 
+// Keeping track of empty tiles
+var empty_tiles = [1,1,1,1, 1,1,1,1, 1,1,1,1, 1,1,1,1]
+var num_empties = 16
+
+// A dictionary holding the text equivalents of each number
 var sym:[Int:String] = [0:" ", 1:"1", 2:"2", 4:"4", 8:"8",
                         16:"A", 32:"B", 64:"C", 128:"D", 256:"E",
                         512:"F", 1024:"G", 2048:"H", 4096:"I", 8192:"J"]
 
+// Prints the board.
 func printTheBoard() {
+	
 	for i in 0..<4 {
+		
 		print("-----------------")
 		for j in 0..<4 {
 			let n = board[i][j]
@@ -30,55 +38,83 @@ func printTheBoard() {
 	print("-----------------")
 }
 
+// Checks if a given tile can move in the given direction.
+// Returns 0 if not, 1 if yes, and 2 if it can merge with the adjacent tile.
 func tileCanMove(row:Int, col:Int, direc:String)->Int {
+	
 	switch direc {
+		
 	case "up":
 		if row == 0 {return 0}
 		if board[row-1][col] == 0 {return 1}
 		if board[row-1][col] == board[row][col] {return 2}
 		return 0
+		
 	case "dn":
 		if row == 3 {return 0}
 		if board[row+1][col] == 0 {return 1}
 		if board[row+1][col] == board[row][col] {return 2}
 		return 0
+		
 	case "lt":
 		if col == 0 {return 0}
 		if board[row][col-1] == 0 {return 1}
 		if board[row][col-1] == board[row][col] {return 2}
 		return 0
+		
 	case "rt":
 		if col == 3 {return 0}
 		if board[row][col+1] == 0 {return 1}
 		if board[row][col+1] == board[row][col] {return 2}
 		return 0
+		
 	default:
 		exit(1)
 	}
 }
 
+// Moves or merges a given tile in the given direction.
 func moveTile(row:Int, col:Int, direc:String, val:Int) {
+	
 	switch direc {
+		
 	case "up":
-		board[row-1][col] = val*board[row][col]; break
+		board[row-1][col] = val*board[row][col]
+		empty_tiles[(row-1)*4+col] = 0
+		break
+		
 	case "dn":
-		board[row+1][col] = val*board[row][col]; break
+		board[row+1][col] = val*board[row][col]
+		empty_tiles[(row+1)*4+col] = 0
+		break
+		
 	case "lt":
-		board[row][col-1] = val*board[row][col]; break
+		board[row][col-1] = val*board[row][col]
+		empty_tiles[row*4+(col-1)] = 0
+		break
+		
 	case "rt":
-		board[row][col+1] = val*board[row][col]; break
+		board[row][col+1] = val*board[row][col]
+		empty_tiles[row*4+(col+1)] = 0
+		break
+		
 	default:
 		exit(1)
 	}
+	
 	board[row][col] = 0
+	empty_tiles[row*4+col] = 1
 }
 
-func push(direc:String) {
+// Pushes all tiles in a given direction.
+func pushTiles(direc:String) {
+	
 	let uplt = ["up","lt"].contains(direc) ? true : false
+	
 	var cont = true
 	while cont {
-		cont = false
 		
+		cont = false
 		var i = uplt ? 0 : 3
 		while i != -1 && i != 4 {
 			
@@ -94,6 +130,7 @@ func push(direc:String) {
 					if canmove == 2 {
 						cont = true
 						moveTile(row: i, col: j, direc: direc, val: 2)
+						num_empties += 1
 					}
 				}
 				
@@ -105,41 +142,105 @@ func push(direc:String) {
 	}
 }
 
-func emptySquares() -> Int {
-	return 0
+// Selects a random tile from the set of empty tiles.
+func getRandEmptyTile()->Int {
+	
+	var idx = Int(arc4random_uniform(UInt32(num_empties)))
+	for i in 0..<16 {
+		
+		if(empty_tiles[i] == 1){
+			if idx == 0 {return i}
+			idx -= 1
+		}
+	}
+	return -1
 }
 
-board = [[1,2,4,0],
-		 [1,2,4,0],
-		 [1,2,4,0],
-		 [0,0,0,0]]
-printTheBoard()
-push(direc: "up")
-printTheBoard()
-print()
+// Inserts a new tile into the board. 
+// If no value is given, it will be 1 or 2.
+// If no location is given, it will be a random empty location.
+func insertTile(val:Int = -1, row:Int = -1, col:Int = -1) {
+	
+	let value = (val == -1 ? Int(arc4random_uniform(2)+1) : val)
+	if row == -1 || col == -1 {
+		let loc = getRandEmptyTile()
+		if loc == -1 {return}
+		
+		let c = loc % 4
+		let r = (loc - c)/4
+		
+		board[r][c] = value
+		empty_tiles[loc] = 0
+		
+	} else {
+		board[row][col] = value
+	}
+	
+	num_empties -= 1
+}
 
-board = [[1,1,1,0],
-         [2,2,2,0],
-         [4,4,4,0],
-         [0,0,0,0]]
-printTheBoard()
-push(direc: "lt")
-printTheBoard()
-print()
+// Print the help message
+func printHelp() {
+	print("\nControls:")
+	print("\t- Enter 'w','a','s','d' to push tiles up, left, down, or right, respectively")
+	print("\t- Enter 'q' to quit the game")
+	print("\t- Enter 'h' to show this message again\n")
+}
 
-board = [[1,2,4,0],
-         [1,2,4,0],
-         [1,2,4,0],
-         [0,0,0,0]]
-printTheBoard()
-push(direc: "dn")
-printTheBoard()
-print()
+// Checks if game is over. 
+// Returns result.
+func gameIsOver()->Bool {
+	return false; // TO-DO
+}
 
-board = [[1,1,1,0],
-         [2,2,2,0],
-         [4,4,4,0],
-         [0,0,0,0]]
-printTheBoard()
-push(direc: "rt")
-printTheBoard()
+// Run the game
+func play() {
+	print("Welcome to TEXTY48")
+	printHelp()
+	print("Enjoy!\n")
+
+	insertTile()
+	insertTile()
+	insertTile()
+	printTheBoard()
+	
+	var exit = 0 // 1 = quit, 2 = gameover
+	while exit == 0 {
+		let input = readLine()!
+		switch input {
+		case "w","W":
+			pushTiles(direc: "up")
+			insertTile()
+			break
+		case "a","A":
+			pushTiles(direc: "lt")
+			insertTile()
+			break
+		case "s","S":
+			pushTiles(direc: "dn")
+			insertTile()
+			break
+		case "d","D":
+			pushTiles(direc: "rt")
+			insertTile()
+			break
+		case "q","Q":
+			print("Are you sure you want to quit? (y/n) ", terminator: "")
+			if(["y","Y"].contains(readLine()!)) {exit = 1}
+			break
+		case "h","H":
+			printHelp()
+			break
+		default:
+			print("Invalid input")
+		}
+		if num_empties == 0 && gameIsOver() {
+			exit = 2
+		}
+		printTheBoard()
+	}
+	
+	
+}
+
+play()
